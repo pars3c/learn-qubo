73e28533e58f076ec8feb7d4b5638f5320d3a2b8
Aderito Xavier <aderito_xavier@sapo.pt>
2020-12-12 22:17:54 +0000

    matrix is generated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import QuboSandBox from './Components/QuboSandBox';
+function App() {
+  return (
+    <div className="App">
+      <h1>Practice Qubo</h1>
+      <QuboSandBox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/QuboSandBox.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const QuboForm = ({ onChange }) => {
+  return (
+    <div>
+      <input
+       type="number"
+       placeholder="Amount of Linear Variables"
+       onChange={(event) => onChange(event.target.value)}
+      />
+    </div>
+  )
+}
+// TODO: GIVE BETTER NAMES TO VARIABLES
+const QuboMatrix = (props) => {
+  console.log(props.data.value)
+  let superArray = new Array(props.data.value**2).fill(0)
+  const final = [];
+  superArray = new Array(Math.ceil(superArray.length / props.data.value))
+  .fill()
+  .map(_ => superArray.splice(0, props.data.value))
+  console.log(superArray)
+  
+
+  for (let i = 0; i < superArray.length; i++) {
+
+    final.push(<p>{JSON.stringify(superArray[i])}</p>)
+  }
+  return (
+    <div className="matrix">
+      {final}
+    </div>
+  )
+
+}
+
+class QuboSandBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 1
+    };
+
+  }
+
+  getInput = (data) => {
+    this.setState({value: parseInt(data)})
+    console.log(this.state.value);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>You have {this.state.value}</h1>
+        <QuboForm onChange={this.getInput}/>
+        {(this.state.value>0 && Number.isInteger(this.state.value)) &&
+         <QuboMatrix data={ this.state }/>
+        }
+
+        
+      </div>
+    
+    );
+  }
+}
+
+export default QuboSandBox;
